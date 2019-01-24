3d8fe8345549c88549cc547a111744f7bda22a76
Raileanu Cosmin <comico2013@gmail.com>
2019-01-24 03:09:41 +0200

    Update ProximitySensor.cpp

--- libsensors/ProximitySensor.cpp
+++ libsensors/ProximitySensor.cpp
@@ -76,18 +76,7 @@ int ProximitySensor::setInitialState() {
 
 int ProximitySensor::setDelay(int32_t handle, int64_t ns)
 {
-    int fd;
-
-    strcpy(&input_sysfs_path[input_sysfs_path_len], "prox_poll_delay");
-    fd = open(input_sysfs_path, O_RDWR);
-    if (fd >= 0) {
-        char buf[80];
-        sprintf(buf, "%lld", ns);
-        write(fd, buf, strlen(buf)+1);
-        close(fd);
-        return 0;
-    }
-    return -1;
+	return 0;
 }
 
 int ProximitySensor::enable(int32_t handle, int en) {
