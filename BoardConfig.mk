3081e4353599634454e9416ddef5ae1c692432a9
Raileanu Cosmin <comico2013@gmail.com>
2019-02-22 23:50:15 +0200

    Fix derp
    Thanks @ChronoMonochrome

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ TARGET_KERNEL_CONFIG := lineageos_n7100_defconfig
 
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/n7100/include
 
-TARGET_LD_SHIM_LIBS += /system/vendor/lib/gpsd_shim.so
+TARGET_LD_SHIM_LIBS += /system/vendor/bin/gpsd|gpsd_shim.so
 
 # Cache
 BOARD_CACHEIMAGE_PARTITION_SIZE := 104857600
